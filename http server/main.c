b808e1d7003dbf38a1294ccbee8510d090309094
Denis <d.n.voropaev@gmail.com>
2017-05-27 23:29:39 +0300

    Initial Commit

---
+++ http server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  http server
+//
+//  Created by Denis on 27/05/2017.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
